c96f39203dcb97630649ca06caa2c0df33810db6
mrron313 <ez.boy763@gmail.com>
2022-11-08 19:21:07 +0600

    lng changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
     <a-layout :style="{ minHeight: '100vh', height : '100%', }">
-      <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', color: '#fff' }">
-        <router-link to="/"><h3 :style="{  fontSize: '22px', color: '#fff' }">App Title</h3></router-link>
-        <a-col :span="4" :order="2">
-          <a-select
-            ref="select"
-            style="width: 100%"
-            placeholder="Municipality"
-            v-model="store.lng"
-            @change="handleLanguageChange"
-          >
-            <a-select-option value="ENG">ENG</a-select-option>
-            <a-select-option value="UKR">UKR</a-select-option>
-          </a-select>
-        </a-col>
+      <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', color: '#fff', display: 'flex', 'justify-content': 'space-between', 'align-items': 'center' }">
+        <router-link to="/"><h3 :style="{  fontSize: '22px', color: '#fff', display: 'inline-block' }">App Title</h3></router-link>
+        <a-select
+          @change="handleLanguageChange"
+          v-model:value="store.lng"
+        >
+          <a-select-option selected=true value="ENG">ENG</a-select-option>
+          <a-select-option value="UKR">UKR</a-select-option>
+        </a-select>
       </a-layout-header>
       <a-layout-content :style="{ padding: '0 50px', marginTop: '64px',  background: '#fff', height: '100%',}">
         <div :style="{ padding: '24px', }">
@@ -21,7 +16,7 @@
         </div>
       </a-layout-content>
       <a-layout-footer :style="{ textAlign: 'center' }">
-        Developed By Arif 
+        Developed By Arif
       </a-layout-footer>
     </a-layout>
 </template>
