b53e467d657dccc9316c9b9f1b57036e259a0a04
arif <ez.boy763@gmail.com>
2022-11-13 20:28:20 +0600

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,54 @@
 <template>
-    <a-layout :style="{ minHeight: '100vh', height : '100%', }">
-      <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%', color: '#fff', display: 'flex', 'justify-content': 'space-between', 'align-items': 'center' }">
-        <router-link to="/"><h3 :style="{  fontSize: '22px', color: '#fff', display: 'inline-block' }">{{ labels.Title[store.lng] }}</h3></router-link>
-        <a-select
-          @change="handleLanguageChange"
-          v-model:value="store.lng"
+  <a-layout :style="{ minHeight: '100vh', height: '100%' }">
+    <a-layout-header
+      :style="{
+        position: 'fixed',
+        zIndex: 1,
+        width: '100%',
+        color: '#fff',
+        display: 'flex',
+        'justify-content': 'space-between',
+        'align-items': 'center',
+      }"
+    >
+      <router-link to="/"
+        ><h3
+          :style="{ fontSize: '22px', color: '#fff', display: 'inline-block' }"
         >
-          <a-select-option selected=true value="ENG">ENG</a-select-option>
-          <a-select-option value="UKR">UKR</a-select-option>
-        </a-select>
-      </a-layout-header>
-      <a-layout-content :style="{ marginTop: '64px',  background: '#fff', height: '100%',}">
-        <div :style="{ padding: '24px 0px', }">
-          <router-view></router-view>
-        </div>
-      </a-layout-content>
-      <a-layout-footer :style="{ textAlign: 'center' }">
-      </a-layout-footer>
-    </a-layout>
+          {{ labels.Title[store.lng] }}
+        </h3></router-link
+      >
+      <a-select @change="handleLanguageChange" v-model:value="store.lng">
+        <a-select-option selected="true" value="ENG">ENG</a-select-option>
+        <a-select-option value="UKR">UKR</a-select-option>
+      </a-select>
+    </a-layout-header>
+    <a-layout-content
+      :style="{ marginTop: '64px', background: '#fff', height: '100%' }"
+    >
+      <div :style="{ padding: '24px 0px' }">
+        <router-view></router-view>
+      </div>
+    </a-layout-content>
+    <a-layout-footer :style="{ textAlign: 'center' }"> </a-layout-footer>
+  </a-layout>
 </template>
 
 <script>
-  import { store } from './store.js'
-  import { Labels } from './_dev/data'
+import { store } from "./store.js";
+import { Labels } from "./_dev/data";
 
-  export default {
-    data() {
-      return {
-        store,
-        labels: Labels
-      }
+export default {
+  data() {
+    return {
+      store,
+      labels: Labels,
+    };
+  },
+  methods: {
+    handleLanguageChange(val) {
+      store.increment(val);
     },
-    methods: {
-      handleLanguageChange(val) {
-        store.increment(val)
-      }
-    }
-  }
+  },
+};
 </script>
--- src/components/Listings/LisitingItem.vue
+++ src/components/Listings/LisitingItem.vue
@@ -1,30 +1,36 @@
 <template>
-  <a-col flex="'200px'">
-    <a-card hoverable style="max-width: 300px; minHeight: 120px; margin: 20px" @click="goToDetails(item.id)">
+  <a-col flex="'300px'">
+    <a-card
+      hoverable
+      style="width: 300px; minheight: 120px; margin: 20px"
+      @click="goToDetails(item.id)"
+    >
       <a-card-meta :title="item.AN">
-        <template #description>{{ this.shortentDescription(item.Description[store.lng]) }}</template>
+        <template #description>{{
+          this.shortentDescription(item.Description[store.lng])
+        }}</template>
       </a-card-meta>
     </a-card>
   </a-col>
 </template>
 
 <script>
-  import { store } from '../../store'
+import { store } from "../../store";
 
-  export default {
-    inject: ['shortentDescription'],
-    props: {
-      item: {}
+export default {
+  inject: ["shortentDescription"],
+  props: {
+    item: {},
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    goToDetails(id) {
+      this.$router.push(`/details/${id}`);
     },
-    data() {
-      return {
-        store
-      }
-    },
-    methods: {
-      goToDetails(id) {
-        this.$router.push(`/details/${id}`)
-      },
-    },
-  }
+  },
+};
 </script>
